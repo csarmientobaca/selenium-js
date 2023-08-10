202ea61e030418b80b2e41fc9d0ee0ce97aba14e
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-08-10 20:15:17 +0200

    adde an nor to the condition of the if in the response.statusCode

--- prova/prova.js
+++ prova/prova.js
@@ -6,26 +6,26 @@ const http = require('http');
 const https = require('https');
 
 function assertLinkNotValid(link) {
-    const httpModule = link.startsWith('https') ? require('https') : require('http'); // Require the 'https' or 'http' module
+    const httpModule = link.startsWith('https') ? require('https') : require('http');
 
     return new Promise((resolve, reject) => {
         httpModule.get(link, (response) => {
-            console.log("------response:", response);
-
-            if (response.statusCode === 200) {
+            // console.log("------response:", response);
+            console.log(response.statusCode)
+            if (response.statusCode === 200 || 301) {
                 console.log("------response 200");
 
                 try {
-                    const responseUrl = response.responseUrl || link; // Use link if responseUrl is undefined
-                    console.log(link);
+                    const responseUrl = response.responseUrl || link;
+                    // console.log(link);
                     const responseParsedUrl = new URL(responseUrl);
-                    console.log(responseParsedUrl);
+                    // console.log(responseParsedUrl);
 
                     if (response.headers['x-cache'] === 'Error from cloudfront') {
                         console.log('Link is invalid due to CloudFront error.');
                         reject(new Error('Link is not a valid route.'));
                     } else if (responseParsedUrl.pathname === new URL(link).pathname) {
-                        console.log('Link is valid.insideeee');
+                        console.log('Valid link => ', link);
                         resolve();
                     } else {
                         console.log('Link redirects to another route. Not a valid route.');
@@ -36,7 +36,7 @@ function assertLinkNotValid(link) {
                     reject(error);
                 }
             } else {
-                console.log('Link is invalid.');  // Print when the link returns a non-200 status code
+                console.log('not a valid response');  // Print when the link returns a non-200 nor 301 status code
                 resolve();  // Resolve for invalid link
             }
         }).on('error', (error) => {
@@ -46,103 +46,104 @@ function assertLinkNotValid(link) {
     });
 }
 
-// Example usage
-const inputUrl = 'https://miniclip.com/games/web';
+// check the link if is valid
+const inputUrl = 'https://miniclip.com/games';
 
 assertLinkNotValid(inputUrl)
     .then(() => {
-        console.log('Link is valid.out');
     })
     .catch((error) => {
         console.error('Assertion failed:', error.message);
     });
 
-// async function prova() {
-//     let driver = await new Builder().forBrowser("chrome").build();
 
-//     try {
-//         // Go to miniclip
-//         await driver.get("https://miniclip.com/");
+///second function
+async function prova() {
+    let driver = await new Builder().forBrowser("chrome").build();
 
-//         // Find the <span> element with the text "Accept" and click on it
-//         let acceptButton = await driver.findElement(By.xpath('//span[text()="Accept"]'));
-//         await acceptButton.click();
+    try {
+        // Go to miniclip
+        await driver.get("https://miniclip.com/");
 
-//         // Go to /GAMES ROUTE
-//         let gamesLink = await driver.findElement(By.linkText('Games'));
-//         await gamesLink.click();
+        // Find the <span> element with the text "Accept" and click on it
+        let acceptButton = await driver.findElement(By.xpath('//span[text()="Accept"]'));
+        await acceptButton.click();
 
-//         let totalFeaturedEntries = 0;
+        // Go to /GAMES ROUTE
+        let gamesLink = await driver.findElement(By.linkText('Games'));
+        await gamesLink.click();
 
-//         while (true) {
-//             await driver.wait(until.titleContains("Games"), 10000);
+        let totalFeaturedEntries = 0;
 
-//             let featuredEntryElements = await driver.findElements(By.css('.featured-entry'));
-//             let numberOfFeaturedEntries = featuredEntryElements.length;
-//             totalFeaturedEntries += numberOfFeaturedEntries;
-//             console.log('Number of games inside loop:', totalFeaturedEntries);
+        while (true) {
+            await driver.wait(until.titleContains("Games"), 10000);
 
-//             try {
-//                 let nextPageLink = await driver.findElement(By.xpath('//a[contains(text(), "Next Page")]'));
-//                 await nextPageLink.click();
+            let featuredEntryElements = await driver.findElements(By.css('.featured-entry'));
+            let numberOfFeaturedEntries = featuredEntryElements.length;
+            totalFeaturedEntries += numberOfFeaturedEntries;
+            console.log('Number of games inside loop:', totalFeaturedEntries);
 
-//                 // Wait for the new page to load
-//                 await driver.wait(until.stalenessOf(nextPageLink));
-//             } catch (error) {
-//                 if (error.name === "NoSuchElementError") {
-//                     console.log("---non ce piu: 'Next Page'---");
-//                     break;
-//                 } else {
-//                     throw error;
-//                 }
-//             }
-//         }
+            try {
+                let nextPageLink = await driver.findElement(By.xpath('//a[contains(text(), "Next Page")]'));
+                await nextPageLink.click();
 
-//         console.log('numero totale di giochi:', totalFeaturedEntries);
+                // Wait for the new page to load
+                await driver.wait(until.stalenessOf(nextPageLink));
+            } catch (error) {
+                if (error.name === "NoSuchElementError") {
+                    console.log("---non ce piu: 'Next Page'---");
+                    break;
+                } else {
+                    throw error;
+                }
+            }
+        }
 
-//         // Go to /careers ROUTE
-//         let careersLink = await driver.findElement(By.linkText('Careers'));
-//         await careersLink.click();
+        console.log('numero totale di giochi:', totalFeaturedEntries);
 
-//         // brute force it to win
-//         let genoaSelect;
-//         let retries = 0;
-//         const maxRetries = 3;
+        // Go to /careers ROUTE
+        let careersLink = await driver.findElement(By.linkText('Careers'));
+        await careersLink.click();
 
-//         while (retries < maxRetries) {
-//             try {
-//                 genoaSelect = await driver.findElement(By.xpath('//option[text()="Genoa, Italy"]'));
-//                 await genoaSelect.click();
-//                 break;
-//             } catch (error) {
-//                 console.log('provando.....');
-//                 retries++;
-//                 await driver.sleep(1000);
-//             }
-//         }
+        // brute force it to win
+        let genoaSelect;
+        let retries = 0;
+        const maxRetries = 3;
 
-//         if (!genoaSelect) {
-//             console.log('niente, nonce la opzione.');
-//         } else {
-//             console.log('Genoa Selezionato');
-//         }
+        while (retries < maxRetries) {
+            try {
+                genoaSelect = await driver.findElement(By.xpath('//option[text()="Genoa, Italy"]'));
+                await genoaSelect.click();
+                break;
+            } catch (error) {
+                console.log('provando.....');
+                retries++;
+                await driver.sleep(1000);
+            }
+        }
 
-//         await driver.wait(until.titleContains("Vacancies"), 10000);
+        if (!genoaSelect) {
+            console.log('niente, nonce la opzione.');
+        } else {
+            console.log('Genoa Selezionato');
+        }
 
-//         // Count the number of elements with the "truncate" class
-//         let truncateElement = await driver.findElements(By.css('.truncate'));
+        await driver.wait(until.titleContains("Vacancies"), 10000);
 
-//         let numberOfTruncate = truncateElement.length;
+        // Count the number of elements with the "truncate" class
+        let truncateElement = await driver.findElements(By.css('.truncate'));
 
-//         console.log('numeri di posizioni aperte a genova:', numberOfTruncate);
+        let numberOfTruncate = truncateElement.length;
 
-//         //quanto tempo rimane aperto
-//         await new Promise(resolve => setTimeout(resolve, 5000));
-//     } catch (error) {
-//         console.error("---->THIS IS AN ERROR <----:", error);
-//     } finally {
-//         await driver.quit();
-//     }
-// }
+        console.log('numeri di posizioni aperte a genova:', numberOfTruncate);
 
-// prova();
+        //quanto tempo rimane aperto
+        await new Promise(resolve => setTimeout(resolve, 5000));
+    } catch (error) {
+        console.error("---->THIS IS AN ERROR <----:", error);
+    } finally {
+        await driver.quit();
+    }
+}
+
+prova();
