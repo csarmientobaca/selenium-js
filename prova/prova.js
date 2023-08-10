3145dc9d6fd8615a0cd7db7c089ec5b0015e9085
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-08-10 04:47:35 +0200

    added the click of the next page

--- prova/prova.js
+++ prova/prova.js
@@ -1,3 +1,4 @@
+const { lstat } = require("fs");
 const { By, Key, Builder, until } = require("selenium-webdriver");
 require("chromedriver");
 
@@ -9,14 +10,42 @@ async function prova() {
         //RICORDARE DI FARLO COMPATIBILE CON FIREFOX E CRHOME
         //SE SI PUO
 
-        // Go to Google
+        // Go to miniclip
         await driver.get("https://miniclip.com/");
 
+        // Find the <span> element with the text "Accept"
+        let acceptButton = await driver.findElement(By.xpath('//span[text()="Accept"]'));
+
+        // Click on the <span> element
+        await acceptButton.click();
 
 
+        //ASSERT THAT THE LINK IS NOT VALID WITH A HHTPS RESPONSE
 
 
+        //////////////////////////
 
+
+        //GO TO /GAMES ROUTE
+        let gamesLink = await driver.findElement(By.linkText('Games'));
+        await gamesLink.click();
+
+
+        // WAIT FOR THE PAGE TO LOAD
+        await driver.wait(until.titleContains("Games"), 3000);
+
+        // COUNT THE NUMBER OF <article> ELEMENTS WITHIN THE <section>
+        let featuredEntryElements = await driver.findElements(By.css('.featured-entry'));
+        let numberOfFeaturedEntries = + featuredEntryElements.length;
+
+
+        let nextPageLink = await driver.findElement(By.xpath('//a[contains(text(), "Next Page")]'));
+        await driver.wait(until.elementIsEnabled(nextPageLink));
+        await nextPageLink.click();
+        // COUNT THE NUMBER OF <article> ELEMENTS WITHIN THE <section>
+
+
+        console.log('Number of games:', numberOfFeaturedEntries);
 
         ////this is google practice
 
@@ -45,7 +74,7 @@ async function prova() {
 
 
         // Wait for a specified amount of time (e.g., 20 seconds) before quitting
-        await new Promise(resolve => setTimeout(resolve, 20000));
+        await new Promise(resolve => setTimeout(resolve, 3000000));
     } catch (error) {
         console.error("---->THIS IS AN ERROR <----:", error);
     } finally {
